a9a98311a3b83baf3b23a9362ff480db526f56bc
andmarek <andmarek@outlook.com>
2020-03-14 18:40:54 -0500

    moving things

--- tests/queue.h
+++ tests/queue.h
@@ -2,18 +2,12 @@
 #define QUEUE_H
 
 #include <stdint.h>
-/*
-struct queue_node;
-typedef struct queue_node queue_node_t;*/
 
 typedef struct queue_node {
     struct queue_node *next;
     void *datum;
 } queue_node_t;
-/*typedef struct queue_node {
-    struct queue_node *next;
-    int datum;
-} queue_node_t;*/
+
 
 typedef struct queue {
     struct queue_node *head;
